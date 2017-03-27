58eba94f62d9f28435c2143f266d624befae1d8c
Srikanth Adavalli <adavalli123@gmail.com>
2017-03-26 23:06:54 -0500

    first commit

---
+++ Virtusa Image Album/VAlbumTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  VAlbumTableViewCell.swift
+//  Virtusa Image Album
+//
+//  Created by Srikanth Adavalli on 3/23/17.
+//  Copyright © 2017 Srikanth Adavalli. All rights reserved.
+//
+
+import Foundation
---
+++ Virtusa Image AlbumTests/Virtusa_Image_AlbumTests.swift
@@ -0,0 +1,36 @@
+//
+//  Virtusa_Image_AlbumTests.swift
+//  Virtusa Image AlbumTests
+//
+//  Created by Srikanth Adavalli on 3/23/17.
+//  Copyright © 2017 Srikanth Adavalli. All rights reserved.
+//
+
+import XCTest
+@testable import Virtusa_Image_Album
+
+class Virtusa_Image_AlbumTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
